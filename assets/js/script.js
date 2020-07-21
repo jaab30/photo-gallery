315190903e9b0f45cd0e808842f451efdf997e1e
jaab30 <jaab30@yahoo.com>
2020-07-21 16:26:06 -0400

    Loding gif added, scroll effect nav added, reconfigured render photos layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,79 @@
 
 
-const getPictures = async () => {
-
-    const response = await $.get("https://api.unsplash.com/search/photos?page=1&query=drums&client_id=urPpuulEpzzAkhzft7TJA5Q6UEyyDL9tTaALLgnWsz8&per_page=27");
-
-    displayResults(response);
-}
-
-const displayResults = (response) => {
-    for (let i = 0; i < response.results.length; i++) {
-        const { urls } = response.results[i];
-        if (i < 9) {
-            let photoImg = $("<img>").attr("src", urls.regular)
-            $(".col-1").append(photoImg);
-            console.log("8");
-        } else if (i >= 9 && i < 18) {
-            let photoImg = $("<img>").attr("src", urls.regular)
-            $(".col-2").append(photoImg);
-        } else {
-            let photoImg = $("<img>").attr("src", urls.regular)
-            $(".col-3").append(photoImg);
-        }
+const listenScrollEvent = e => {
+    if (window.scrollY < 200) {
+        $("header").removeClass("header-scroll");
+    } else {
+        $("header").addClass("header-scroll");
     }
 }
 
-getPictures();
+window.addEventListener('scroll', listenScrollEvent)
 
+
+
+
+const getQuery = (e) => {
+    e.preventDefault();
+    const query = $(".search-input").val().trim();
+    if (!query) return $(".alert-mssg").html(`Please Enter a Search Term &#8594;`);
+    getPhotos(query)
+    $(".search-input").val("");
+    $(".alert-mssg").text("");
+    $(".status-mssg").text("");
+
+}
+
+const getPhotos = async (query) => {
+
+    const response = await $.get(`https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=urPpuulEpzzAkhzft7TJA5Q6UEyyDL9tTaALLgnWsz8&per_page=27`);
+
+    displayResults(response, query);
+    window.location.href = "#banner";
+}
+
+const renderPhoto = (url, altInfo) => {
+    return (
+        `<div class="photo-background">
+            <img src=${url} alt="${altInfo}" title="${altInfo}">
+         </div>
+        `
+    )
+}
+
+const displayResults = (response, query) => {
+    console.log(response.results.length);
+    $(".col-1").empty();
+    $(".col-2").empty();
+    $(".col-3").empty();
+    if (response.results.length === 0) {
+        $(".status-mssg").text(`Could not find any records for ${query}. Please, try again... Thanks..!`)
+    }
+    let colIndex = 1;
+    for (let i = 0; i < response.results.length; i++) {
+        const { urls, alt_description } = response.results[i];
+        console.log(colIndex);
+        $(".col-" + colIndex).append(renderPhoto(urls.small, alt_description));
+        if (colIndex === 3) {
+            colIndex = 0
+        }
+        colIndex++
+    }
+}
+
+$("#search-form").on("submit", getQuery)
+
+// console.log(colIndex);
+// if (i < 9) {
+//     $(".col-1").append(renderPhoto(urls.small, alt_description));
+// } else if (i >= 9 && i < 18) {
+//     $(".col-2").append(renderPhoto(urls.small, alt_description));
+// } else {
+//     $(".col-3").append(renderPhoto(urls.small, alt_description));
+// }
+// $(".photo-container").append(renderPhoto(urls.small, alt_description));
+
+// $(".submit-btn").on("click", getQuery)
 
 
 
